03051979b076ed2b15e429cbea50f6e0c0ff668b
Hippolyte Einfalt <hippolyteeinfalt@hippolytes-mbp.home>
2017-03-21 00:13:35 +0100

    lol

--- ft_putnstr.c
+++ ft_putnstr.c
@@ -1,19 +1,16 @@
 #include "libft.h"
 
-void	ft_putnstr(const char *str, const int n)
+void	ft_putnstr(const char *str, int n)
 {
 	int i;
 
 	i = 0;
 	if (str != NULL)
 	{
-		if (n <= 0)
-			ft_putstr(str);
-		else
+		if (n > 0)
 		{
 			while (str[i] && i < n)
 				ft_putchar(str[i++]);
 		}
 	}
 }
-
